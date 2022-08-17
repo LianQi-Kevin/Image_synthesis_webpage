d79ec9da6a60f8e600b84fc2fc47cfbaa009d434
LianQi-Kevin <18201584863@163.com>
2022-08-17 13:29:46 +0800

    function collation

--- page/txt2img_refactor.py
+++ page/txt2img_refactor.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import time
 from contextlib import nullcontext
 from itertools import islice
 
@@ -48,12 +47,12 @@ def make_args():
 
     parser.add_argument("--prompt", type=str, nargs="?", default="a painting of a virus monster playing guitar",
                         help="the prompt to render")
-    parser.add_argument("--outdir", type=str, nargs="?", help="dir to write results to",
+    parser.add_argument("--out_dir", type=str, nargs="?", help="dir to write results to",
                         default="outputs/txt2img-samples")
     parser.add_argument("--skip_grid", action='store_true',
                         help="do not save a grid, only individual samples. Helpful when evaluating lots of samples", )
     parser.add_argument("--skip_save", action='store_true',
-                        help="do not save indiviual samples. For speed measurements.", )
+                        help="do not save individual samples. For speed measurements.", )
     parser.add_argument("--ddim_steps", type=int, default=50, help="number of ddim sampling steps", )
     parser.add_argument("--plms", action='store_true', help="use plms sampling", )
     parser.add_argument("--fixed_code", action='store_true',
@@ -79,8 +78,8 @@ def make_args():
     parser.add_argument("--ckpt", type=str, default="logs/f8-kl-clip-encoder-256x256-run1/checkpoints/last.ckpt",
                         help="path to checkpoint of model", )
     parser.add_argument("--seed", type=int, default=42, help="the seed (for reproducible sampling)", )
-    parser.add_argument("--precision", type=str, help="evaluate at this precision", choices=["full", "autocast"],
-                        default="autocast")
+    parser.add_argument("--precision", type=str, help="evaluate at this precision", choices=["full", "auto_cast"],
+                        default="auto_cast")
     return parser.parse_args()
 
 
@@ -116,14 +115,13 @@ def text2img(opt, model, device):
     if opt.fixed_code:
         start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
 
-    precision_scope = autocast if opt.precision == "autocast" else nullcontext
+    precision_scope = autocast if opt.precision == "auto_cast" else nullcontext
     # 使用with代换try/finally
     with torch.no_grad():
         with precision_scope("cuda"):
             with model.ema_scope():
-                tic = time.time()
                 all_samples = list()
-                for n in trange(opt.n_iter, desc="Sampling"):
+                for _ in trange(opt.n_iter, desc="Sampling"):
                     for prompts in tqdm(data, desc="data"):
                         uc = None
                         if opt.scale != 1.0:
@@ -146,29 +144,26 @@ def text2img(opt, model, device):
                         x_samples_ddim = model.decode_first_stage(samples_ddim)
                         x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
 
-                        # 单张图
-                        # if not opt.skip_save:
-                        #     for x_sample in x_samples_ddim:
-                        #         x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                        #         Image.fromarray(x_sample.astype(np.uint8)).save(
-                        #             os.path.join(sample_path, f"{base_count:05}.png"))
-                        #         base_count += 1
                         all_samples.append(x_samples_ddim)
-
-                toc = time.time()
 
     return all_samples
 
 
-def postprocess(all_samples, n_rows):
-    # additionally, save as grid
-    grid = torch.stack(all_samples, 0)
-    grid = rearrange(grid, 'n b c h w -> (n b) c h w')
-    grid = make_grid(grid, nrow=n_rows)
-
-    # to image
-    grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-    return Image.fromarray(grid.astype(np.uint8))
+def postprocess(all_samples, opt, single=False):
+    if single:
+        images_list = list()
+        for x_samples_ddim in all_samples:
+            for x_sample in x_samples_ddim:
+                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                images_list.append(Image.fromarray(x_sample.astype(np.uint8)))
+        return images_list
+    else:
+        n_rows = opt.n_rows if opt.n_rows > 0 else opt.n_samples
+        grid = torch.stack(all_samples, 0)
+        grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+        grid = make_grid(grid, nrow=n_rows)
+        grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
+        return Image.fromarray(grid.astype(np.uint8))
 
 
 def main():
@@ -177,16 +172,21 @@ def main():
 
     # ----------
     # 调试用 覆盖args
-    opt.prompt = "sunset, sunrays showing through the woods in front, clear night sky, stars visible, mountain in the back, lake in front reflecting the night sky and mountain, photo realistic, 8K, ultra high definition, cinematic"
+    # opt.prompt = "sunset, sun rays showing through the woods in front, clear night sky, stars visible, mountain in the back, lake in front reflecting the night sky and mountain, photo realistic, 8K, ultra high definition, cinematic"
+    opt.prompt = "A stained glass window of a panda eating bamboo, photo realistic, 8K, ultra high definition, cinematic"
     opt.config = "/root/latent-Diffusion-Models/configs/latent-diffusion/txt2img-1p4B-eval.yaml"
     opt.ckpt = "/root/latent-Diffusion-Models/models/ldm/text2img-large/model.ckpt"
-    opt.ddim_eta = 0.0
-    opt.n_samples = 3
-    opt.n_iter = 3
-    opt.scale = 5.0
-    opt.ddim_steps = 50
-    opt.skip_save = True
-    opt.seed = np.random.randint(1, 100)    # seed相同时结果相同
+    opt.ddim_eta = 0.0  # 0.0 corresponds to deterministic sampling
+    opt.n_samples = 3   # batch size    # X
+    opt.n_iter = 3      # Y
+    opt.scale = 5.0     # https://benanne.github.io/2022/05/26/guidance.html
+    opt.ddim_steps = 50     # ddim sampling steps
+    opt.out_dir = "outputs/txt2img-samples"     # output dir
+    opt.H = 256     # Must be divisible by 8
+    opt.W = 256     # Must be divisible by 8
+    opt.skip_save = True    # skip save single img
+    opt.skip_grid = False   # skip save grid img
+    opt.seed = np.random.randint(1, 100)    # The same results when the seeds are the same
     # ----------
 
     seed_everything(opt.seed)
@@ -195,8 +195,8 @@ def main():
     model, device = load_model(opt)
 
     # output path
-    os.makedirs(opt.outdir, exist_ok=True)
-    out_path = opt.outdir
+    os.makedirs(opt.out_dir, exist_ok=True)
+    out_path = opt.out_dir
 
     # single img msg
     sample_path = os.path.join(out_path, "samples")
@@ -206,11 +206,15 @@ def main():
 
     all_samples = text2img(opt, model, device)
 
-    n_rows = opt.n_rows if opt.n_rows > 0 else opt.n_samples
+    # 单张图
+    if not opt.skip_save:
+        for img in postprocess(all_samples, opt, single=True):
+            img.save(os.path.join(sample_path, f"{base_count:05}.png"))
+            base_count += 1
 
     # 网格图
     if not opt.skip_grid:
-        postprocess(all_samples, n_rows).save(os.path.join(out_path, f'grid-{grid_count:04}.png'))
+        postprocess(all_samples, opt, single=False).save(os.path.join(out_path, f'grid-{grid_count:04}.png'))
 
 
 if __name__ == "__main__":
