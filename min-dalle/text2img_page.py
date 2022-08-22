943b49a5e96a8dbb7e194d7b8210735343b10bd1
LianQi-Kevin <18201584863@163.com>
2022-08-22 20:36:10 +0800

    add min-dalle examples

--- min-dalle/text2img_page.py
+++ min-dalle/text2img_page.py
@@ -58,7 +58,7 @@ def save_img(images, grid_size=3, save_path="outputs", save_single=True, save_gr
         return f"grid-{grid_basic_i:04}.png"
 
 
-def random_seed(advanced_page):
+def update_random_seed(advanced_page):
     if advanced_page:
         return gr.update(interactive=False, value=-1)
     else:
@@ -90,7 +90,8 @@ def run_model(prompt, seed=-1, grid_size=2, is_seamless=False, temperature=1, to
     logging.info(f"Seed: {seed}, Grid_Size: {grid_size}, Seamless: {is_seamless}, "
                  f"Temperature: {temperature}, Top_k: {top_k}, supercondition_factor: {supercondition_factor}")
     images = [Image.fromarray((img * 1).astype(np.uint8)).convert('RGB') for img in images.to('cpu').numpy()]
-    grid_img_path = save_img(images, grid_size, save_path="./outputs")
+    # grid_img_path = save_img(images, grid_size, save_path="./outputs")
+    grid_img_path = save_img(images, grid_size, save_path="./examples/")
     return images, grid_img_path
 
 
@@ -104,7 +105,7 @@ def gradio_basic_page():
                 with gr.Column():
                     prompt_box = gr.Textbox(label="提示词", lines=1)
                     go_button = gr.Button("开始绘画", elem_id="go_button")
-                    output_img = gr.Gallery(interactive=False, show_label=False).style(grid=[3], height="640px")
+                    output_img = gr.Gallery(interactive=False, show_label=False).style(grid=[5], height="640px")
         # style
         go_button.style(full_width="True")
 
@@ -126,11 +127,11 @@ def gradio_advanced_app():
                     output_gallery = gr.Gallery(interactive=False, show_label=False, elem_id="output_gallery")
                 with gr.Column():
                     gr.Markdown("### Setting")
-                    seed_box = gr.Number(value=np.random.randint(1, 100000), label="Seed",
-                                         interactive=True, precision=0)
+                    seed_box = gr.Number(value=-1, label="Seed",
+                                         interactive=False, precision=0)
                     with gr.Row():
                         grid_size_slider = gr.Slider(value=2, minimum=1, maximum=5, step=1, label="Grid Size")
-                        random_seed_checkbox = gr.Checkbox(value=False, label="Random Seed")
+                        random_seed_checkbox = gr.Checkbox(value=True, label="Random Seed")
                         seamless_checkbox = gr.Checkbox(value=False, label="Seamless")
                     gr.Markdown("#### Advanced")
                     with gr.Row():
@@ -140,8 +141,6 @@ def gradio_advanced_app():
                         supercondition_dropdown = gr.Dropdown(label="Super Condition", value="16",
                                                               choices=[str(2 ** i) for i in range(2, 7)],
                                                               interactive=True)
-                    grid_path_textbox = gr.Textbox(interactive=False, visible=True, label="Grid_img output path")
-
                     gr.Markdown(
                         """
                         ####
@@ -156,21 +155,37 @@ def gradio_advanced_app():
                         """
                     )
 
+            gr.Examples(examples=[
+                ["cat, sticker, illustration, japanese style"],
+                ["cyberpunk city"],
+                ["A magnificent picture never seen before"],
+                ["what it sees as very very beautiful"],
+                ["a new creature"],
+                ["What does the legendary phoenix  look like"],
+                ["Pug hedgehog hybrid"],
+                ["photo realistic, 4K, ultra high definition, cinematic, sea dragon horse"],
+                ["city of coca cola oil painting"],
+                ["dream come true"],
+                ["AI robot teacher and students kid in classroom "],
+                ["wasteland, space station, cyberpunk, giant ship, photo realistic, 8K, ultra high definition, cinematic"],
+                ["sunset, sunrays showing through the woods in front, clear night sky, stars visible, mountain in the back, lake in front reflecting the night sky and mountain, photo realistic, 8K, ultra high definition, cinematic"],
+            ], inputs=[prompt_box], examples_per_page=40)
+
         # style
         go_button.style(full_width="True", rounded=True)
         seed_box.style(rounded=True)
-        output_gallery.style(grid=[2], height="auto")
+        output_gallery.style(grid=[3], height="auto")
 
         # action
-        random_seed_checkbox.change(random_seed, inputs=[random_seed_checkbox], outputs=[seed_box])
+        random_seed_checkbox.change(update_random_seed, inputs=[random_seed_checkbox], outputs=[seed_box])
         prompt_box.submit(run_model,
                           inputs=[prompt_box, seed_box, grid_size_slider, seamless_checkbox,
                                   temperature_slider, top_k_dropdown, supercondition_dropdown],
-                          outputs=[output_gallery, grid_path_textbox])
+                          outputs=[output_gallery])
         go_button.click(run_model,
                         inputs=[prompt_box, seed_box, grid_size_slider, seamless_checkbox,
                                 temperature_slider, top_k_dropdown, supercondition_dropdown],
-                        outputs=[output_gallery, grid_path_textbox])
+                        outputs=[output_gallery])
 
     advanced_app.launch(server_port=6006, share=False, quiet=False, enable_queue=True, show_error=True)
 
