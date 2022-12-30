fe3a4cd0d98aa302a081ef6cf4ecf6810109d1fc
LianQi-Kevin <18201584863@163.com>
2022-12-30 15:59:54 +0800

    Fix script line breaks

--- Taiyi_StableDiffusion/unstep.sh
+++ Taiyi_StableDiffusion/unstep.sh
@@ -1,5 +1,9 @@
 out_dir="/root/Image_synthesis_webpage/Taiyi_StableDiffusion/outputs/"
+model_type="ZH"
 #create output folder
 mkdir -p $out_dir
 #run
-/root/miniconda3/envs/Taiyi/bin/python /root/Image_synthesis_webpage/Taiyi_StableDiffusion/text2img_page.py --out_dir $out_dir --model_type 'ZH' --step_un_show
+/root/miniconda3/envs/Taiyi/bin/python /root/Image_synthesis_webpage/Taiyi_StableDiffusion/text2img_page.py \
+--out_dir $out_dir \
+--model_type $model_type \
+--step_un_show
